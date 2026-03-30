97ddc659c503a2f18ab183d2ce24049a645d7be0
wynzyl <wynzyltayag@gmail.com>
2026-03-30 19:20:30 +0800

    Update serverExternalPackages in next.config.ts and modify build script in package.json to include prisma generate

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   typescript: {ignoreBuildErrors: true},
   allowedDevOrigins: ['https://localhost:3000', 'http://cho.yamban-tech.it:3000'],
-  serverExternalPackages: ['pg', 'bcrypt', '@prisma/adapter-pg'],
+  serverExternalPackages: ['pg', 'bcrypt', '@prisma/client', '@prisma/adapter-pg'],
   // reactCompiler:true,
   // experimental:{turbopackFileSystemCacheForBuild :true}
 };
