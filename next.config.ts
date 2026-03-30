28c09ae62770c4d94093b8c16b8a139fbfc2be66
wynzyl <wynzyltayag@gmail.com>
2026-03-30 17:03:29 +0800

    Add typescript configuration to ignore build errors in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  typescript: {ignoreBuildErrors: true},
   allowedDevOrigins: ['https://localhost:3000', 'http://cho.yamban-tech.it:3000'],
   // reactCompiler:true,
   // experimental:{turbopackFileSystemCacheForBuild :true}
