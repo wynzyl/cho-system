f31bf4eda31a3e89a80c8f0e0e2feff05928ce79
wynzyl <wynzyltayag@gmail.com>
2026-03-30 11:46:05 +0800

    Update next.config.ts to include allowedDevOrigins, enable reactCompiler, and set experimental options for turbopack file system cache.

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: ['https://localhost:3000', 'http://cho.yamban-tech.it:3000'],
+  // reactCompiler:true,
+  // experimental:{turbopackFileSystemCacheForBuild :true}
 };
 
 export default nextConfig;
